1b4686af4b815ca62f55ebcfe0d014cb3820f3fc
willmustang <williandesouza3@gmail.com>
2024-12-18 16:29:59 -0300

    Atualização18/12-16:29

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,10 @@
+const { defineConfig } = require('cypress');
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    // other settings from cypress.json
+  },
+});
 module.exports = {
   e2e: {
     setupNodeEvents(on, config) {
